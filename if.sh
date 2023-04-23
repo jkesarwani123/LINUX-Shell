a13915d7fe42e5dd5139902ce597b2325047aacb
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 19:17:32 -0400

    cart

--- if.sh
+++ if.sh
@@ -1,10 +1,22 @@
 fruit_name=$1
 quantity=$2
 
-# shellcheck disable=SC1069
+if [ -z "$fruit_name" ]; then
+  echo Input Missing
+  exit
+fi
+
 if [ "$fruit_name" == "mango" ]
 then
-  echo fruit is $quantity
-  else
-    echo fruit does not exist
-    fi
+  echo Mango Quantity - $quantity
+else
+  echo Fruit does not exist
+fi
+
+## Always double quote variables in expressions
+
+if [ "$quantity" -gt 100 ]; then
+  echo Mango Price - 0.5$
+else
+  echo Mango Price - 1$
+fi
