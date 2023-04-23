f38d0dc5ad250ac337502d92bf6923c8d4daca77
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 13:19:37 -0400

    cart

--- Sample.sh
+++ Sample.sh
@@ -5,3 +5,17 @@ echo name is ${name}
 SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 echo $SCRIPT, $SCRIPTPATH
+
+DATE=$(date +%F)
+echo Today date is ${DATE}
+
+ARTH=$((2-3*4/2))
+
+echo ARTH = ${ARTH}
+
+# Special Variables for Inputs
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
