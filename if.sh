ad15932ead0e1a02fba6b3b72997448642db908d
Juhi K <jkesarwani.us@gmail.com>
2023-04-23 18:47:43 -0400

    cart

--- if.sh
+++ if.sh
@@ -1,9 +1,9 @@
 fruit_name=mango
 quantity=2
 
-if[$fruit_name=="mango"]
+if[ $fruit_name == "mango" ]
 then
-  echo fruit is ${quantity}
+  echo fruit is $quantity
   else
     echo fruit does not exist
     fi
